5b188d2a2c2c5a64a0365f3cca7428decfbb1cac
suh <shihabdinajpur@gmail.com>
2025-01-14 21:43:09 +0600

    30% scroll and removed anchor from link

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const backtotoplink = document.getElementById('gotop')
+const backtotoplink = document.getElementById("gotop");
 const onScroll = () => {
-  const scroll = document.documentElement.scrollTop
-  if (scroll > 0) {
+  const scroll = document.documentElement.scrollTop;
+  const threshold = window.innerHeight * 0.3;
+  if (scroll > threshold) {
     backtotoplink.classList.add("active");
   } else {
-    backtotoplink.classList.remove("active")
+    backtotoplink.classList.remove("active");
   }
-}
-window.addEventListener('scroll', onScroll)
+};
+window.addEventListener("scroll", onScroll);
+
+backtotoplink.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
